3ddebd166aa0689fff97bcc04f32f07283e96244
MarlieRamirez <marlie.lucia@hotmail.com>
2025-05-19 10:06:21 -0600

    Setup deployment

--- config/connect.js
+++ config/connect.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 
 // Create the connection to database
 export const connection = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password:'mysqlMarlie',
-  database: 'movie_theather'
+  host: process.env.MYSQLHOST || 'localhost',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPORT || 'mysqlMarlie',
+  database: process.env.MYSQLDATABASE || 'movie_theather'
 });
